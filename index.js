44fd4071f48d6a16a9a1bd56de6d184ddf339716
Renz <renzgabuay@gmail.com>
2025-08-27 14:19:50 +0800

    Add DOM element references for Expense Tracker
    Select key elements including balance, income, expenses, transaction
    list, form, description input, and amount input to prepare for dynamic
    interaction.

---
+++ index.js
@@ -0,0 +1,7 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income");
+const expensesAmountEl = document.getElementById("expenses");
+const transactionEl = document.getElementById("transaction-lists");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
